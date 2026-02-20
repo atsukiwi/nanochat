1a13215269469f4f89d3058b9cd1a1e4f4e00349
atsukiwi <atsuki.hattori@daedalus.co.jp>
2026-02-21 06:06:57 +0900

    batchを8にした

--- runs/train_gpu1.sh
+++ runs/train_gpu1.sh
@@ -84,7 +84,7 @@ $TASKSET torchrun --standalone --nproc_per_node=1 \
     -m scripts.base_train -- \
     --depth=26 \
     --target-param-data-ratio=8.25 \
-    --device-batch-size=16 \
+    --device-batch-size=8 \
     --save-every=50 \
     --run="$WANDB_RUN"
 
